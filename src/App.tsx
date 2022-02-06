133b546a97f0b0fd5a3515344c609692817050a2
James Feigel <james.feigel@gmail.com>
2022-02-06 17:43:39 -0600

    Moved 'homepage' to .env to fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,14 @@ import Routes from 'Routes';
 
 function App() {
   return (
-    <Box id="main" display="flex" flexDirection="column" flex="1 0 auto">
-      <Box position="relative" display="flex" flexDirection="column" flex="1">
-        <Box
-          display="flex"
-          flexDirection="column"
-          flex="1"
-          alignItems="stretch"
-        >
-          <Box
-            id="content"
-            display="flex"
-            flexDirection="column"
-            flex="1"
-            minHeight="100%"
-            overflow="hidden"
-            m={2.5}
-          >
-            <Routes />
-          </Box>
-        </Box>
-      </Box>
+    <Box
+      id="main"
+      position="relative"
+      display="flex"
+      flexDirection="column"
+      flex="1 0 auto"
+    >
+      <Routes />
     </Box>
   );
 }
